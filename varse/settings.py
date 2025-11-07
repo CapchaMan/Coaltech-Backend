d9c7045b76327a5628d1e7e311e334dfb48c2fdd
Anthony Ogoh <ogohchineduanthony@gmail.com>
2025-11-08 00:00:11 +0100

    Configured render.yaml and whitenoise for deployment

--- varse/settings.py
+++ varse/settings.py
@@ -7,9 +7,10 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-your-secret-key-here'
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['coaltech-backend.onrender.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://coaltech-backend.onrender.com']
 
 
 # -------------------------------
@@ -37,12 +38,15 @@ INSTALLED_APPS = [
 # -------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+
 ]
 
 # -------------------------------
